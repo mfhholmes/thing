2044f9c650c94a340651abfe309faf73975631e6
marcus chromebook <mh@marcusholmes.biz>
2014-10-04 20:20:32 +0800

    fix labels

--- thing.js
+++ thing.js
@@ -1,87 +1,5 @@
 "use strict";
 
-function leftEdge(x,y){
-	var self = this;
-	self.x = x;
-	self.y = y;
-	self.draw = function(map){
-		self.element = $("<div class='loc'></div>")
-		.appendTo(map.element)
-		.css({
-			"background-image":"url(img/left.svg)",
-			"height":map.mh,
-			"width":map.mw,
-			"left":self.x *map.mw,
-			"top":self.y*map.mh
-		});
-	}
-}
-function topEdge(x,y){
-	var self = this;
-	self.x = x;
-	self.y = y;
-	self.draw = function(map){
-		self.element = $("<div class='loc'></div>")
-		.appendTo(map.element)
-		.css({
-			"background-image":"url(img/top.svg)",
-			"height":map.mh,
-			"width":map.mw,
-			"left":self.x *map.mw,
-			"top":self.y*map.mh
-		});
-	}
-}
-function rightEdge(x,y){
-	var self = this;
-	self.x = x;
-	self.y = y;
-	self.draw = function(map){
-		self.element = $("<div class='loc'></div>")
-		.appendTo(map.element)
-		.css({
-			"background-image":"url(img/right.svg)",
-			"height":map.mh,
-			"width":map.mw,
-			"left":self.x *map.mw,
-			"top":self.y*map.mh
-		});
-	}
-}
-function bottomEdge(x,y){
-	var self = this;
-	self.x = x;
-	self.y = y;
-	self.draw = function(map){
-		self.element = $("<div class='loc'></div>");
-		self.element.appendTo(map.element)
-		.css({
-			"background-image":"url(img/bottom.svg)",
-			"height":map.mh,
-			"width":map.mw,
-			"left":self.x *map.mw,
-			"top":self.y*map.mh
-		});
-	}
-}
-function newLoc(x,y){
-	var self = this;
-	self.x = x;
-	self.y = y;
-	self.draw = function(map){
-		self.element = $("<div class='loc'></div>");
-		self.element.appendTo(map.element)
-			.css({"background-image":"url(img/empty.svg)",
-				"z-index":"50",
-				"height":map.mh,
-				"width":map.mw,
-				"left":self.x *map.mw,
-				"top":self.y*map.mh
-			});
-		self.element.on("mouseover",function(){self.element.text (self.x+","+self.y)});
-		self.element.on("mouseout",function(){self.element.text("")});			
-	}		
-}
 
 function thing(){
 	var self = this;
@@ -89,12 +7,12 @@ function thing(){
 	self.actionables = [];
 	self.map = {element:$("#map"),mh:50,mw:50};
 	self.scale = 1;
-	function init(mx,my){
+	self.init = function (mx,my){
 		self.drawables = [];
 		self.actionables = [];
 		for(var x = 0;x<mx;x++){
 			for(var y =0;y<my;y++){
-				self.drawables.push(new newLoc(x,y)); 
+				self.drawables.push(new location(x,y)); 
 				if(x == 0){self.drawables.push(new leftEdge(x,y))};
 				if(x == mx-1){self.drawables.push(new rightEdge(x,y))};
 				if(y == 0){self.drawables.push(new topEdge(x,y))};
@@ -104,9 +22,8 @@ function thing(){
 		//TODO: work out how to not set the width and height specifically
 		self.map.element.width(mx * self.map.mw).height(my * self.map.mh);
 	};
-	init(12,8);
 
-	function drawMap(){
+	self.drawMap = function (){
 		for(var x = 0; x < self.drawables.length;x++){
 			if( "draw" in self.drawables[x]){
 				self.drawables[x].draw(self.map);
@@ -115,6 +32,90 @@ function thing(){
 			}
 		}
 	};
-	drawMap();
+
+	function leftEdge(x,y){
+		var self = this;
+		self.x = x;
+		self.y = y;
+		self.draw = function(map){
+			self.element = $("<div class='loc'></div>")
+			.appendTo(map.element)
+			.css({
+				"background-image":"url(img/left.svg)",
+				"height":map.mh,
+				"width":map.mw,
+				"left":self.x *map.mw,
+				"top":self.y*map.mh
+			});
+		}
+	}
+	function topEdge(x,y){
+		var self = this;
+		self.x = x;
+		self.y = y;
+		self.draw = function(map){
+			self.element = $("<div class='loc'></div>")
+			.appendTo(map.element)
+			.css({
+				"background-image":"url(img/top.svg)",
+				"height":map.mh,
+				"width":map.mw,
+				"left":self.x *map.mw,
+				"top":self.y*map.mh
+			});
+		}
+	}
+	function rightEdge(x,y){
+		var self = this;
+		self.x = x;
+		self.y = y;
+		self.draw = function(map){
+			self.element = $("<div class='loc'></div>")
+			.appendTo(map.element)
+			.css({
+				"background-image":"url(img/right.svg)",
+				"height":map.mh,
+				"width":map.mw,
+				"left":self.x *map.mw,
+				"top":self.y*map.mh
+			});
+		}
+	}
+	function bottomEdge(x,y){
+		var self = this;
+		self.x = x;
+		self.y = y;
+		self.draw = function(map){
+			self.element = $("<div class='loc'></div>");
+			self.element.appendTo(map.element)
+			.css({
+				"background-image":"url(img/bottom.svg)",
+				"height":map.mh,
+				"width":map.mw,
+				"left":self.x *map.mw,
+				"top":self.y*map.mh
+			});
+		}
+	}
+	function location(x,y){
+		var self = this;
+		self.x = x;
+		self.y = y;
+		self.draw = function(map){
+			self.element = $("<div class='loc'></div>");
+			self.element.appendTo(map.element)
+				.css({"background-image":"url(img/empty.svg)",
+					"z-index":"50",
+					"height":map.mh,
+					"width":map.mw,
+					"left":self.x *map.mw,
+					"top":self.y*map.mh
+				});
+			self.element.on("mouseenter",function(){self.element.text(self.x+","+self.y)});
+			self.element.on("mouseout",function(){self.element.text("")});			
+		}		
+	}
 };
 var t = new thing();
+t.init(12,8);
+t.drawMap();
